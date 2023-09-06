8231257bf475f24148159d022a2f8bb13af3038a
svmcoder <124701282+SVMCODER@users.noreply.github.com>
2023-09-06 15:41:01 +0530

    Update s.js

--- JS/s.js
+++ JS/s.js
@@ -38,13 +38,16 @@ var firebaseConfig = {
     }
     productCard.className = 'product-card';
     productCard.innerHTML = `
-    <img class="product-image" src="${product.mainImage}" onclick="window.location.href = 'item.html?request-id=${doc.id}'">
-    <h2 class="product-title" onclick="window.location.href = 'item.html?request-id=${doc.id}'">${product.name}</h2>
-    <div class="product-price" onclick="window.location.href = 'item.html?request-id=${doc.id}'"><div class='op'>${product.discount}</div> ₹${product.price}</div>
-    <div class="product-rate" onclick="window.location.href = 'item.html?request-id=${doc.id}'">${rating}</div>
-    <div class="product-discount" onclick="window.location.href = 'item.html?request-id=${doc.id}'" > FREE SHIPPING</div>
-    <button class="bx bx-cart" onclick="window.location.href = 'item.html?request-id=${doc.id}'"> 𝙱𝚞𝚢</button>
-   `;
+    <img class="product-image" src="${product.mainImage}" onclick="window.location.href = 'item.html?request-id=${product.id}'">
+      <div class='det'>
+      <h2 class="product-title" onclick="window.location.href = 'item.html?request-id=${product.id}'">${product.name}</h2>
+      <div class="product-price" onclick="window.location.href = 'item.html?request-id=${product.id}'"><div class='op'>₹${product.originalPrice}</div> ₹${product.price}</div>
+      <div class="product-rate" onclick="window.location.href = 'item.html?request-id=${product.id}'">${rating}</div>
+      <div class="product-discount" onclick="window.location.href = 'item.html?request-id=${product.id}'" > FREE SHIPPING</div>
+      <button class="bx bx-cart" onclick="window.location.href = 'item.html?request-id=${product.id}'"> 𝙱𝚞𝚢</button>
+    
+      </div>
+    `;
 
     productListElement.appendChild(productCard);
    
