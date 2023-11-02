4b17ddbf7c336a2e132189c1cb89c0f9ab3de810
svmcoder <124701282+SVMCODER@users.noreply.github.com>
2023-11-02 13:16:39 +0530

    Update show.js

--- JS/show.js
+++ JS/show.js
@@ -180,7 +180,7 @@ var x = setInterval(function() {
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
     
   // Output the result in an element with id="demo"
-  document.getElementById("saletime").innerHTML = " Truls Festive Sale starts in "+ days + "d " + hours + "h "
+  document.getElementById("saletime").innerHTML = " Truls Sale starts in "+ days + "d " + hours + "h "
   + minutes + "m " + seconds + "s ";
     
   // If the count down is over, write some text 
